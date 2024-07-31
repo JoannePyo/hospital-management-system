3026446f8f949ceb1178654f11d7b8c473e4609f
JoannePyo <JOANNE.PYO95@qmail.cuny.edu>
2024-07-30 21:01:12 -0400

    updated

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import {cn} from '@/lib/utils'; //cn is class names의 줄인말
+
+const fontSans = Plus_Jakarta_Sans({ 
+  subsets: ["latin"],
+  weight : ['300','400', '500', '600', '700'], //폰트의 두께를 설정하는 부분. thin, regular, medium, semi-Bold, Bold
+  variable: '--font-sans'
+});
+
+export const metadata: Metadata = {
+  title: "CareHeart",
+  description: "A healthcare management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(
+        "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+          )}
+          >
+          {children}
+      </body>
+    </html>
+  );
+}
