0061c1c5fe27077b7dc71cef4ff05f4b65b654ca
JoannePyo <JOANNE.PYO95@qmail.cuny.edu>
2024-08-06 21:35:24 -0400

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import {cn} from '@/lib/utils'; //cn is class names의 줄인말
+import { cn } from "@/lib/utils"; //cn is class names의 줄인말
+import { ThemeProvider } from "next-themes";
 
-const fontSans = Plus_Jakarta_Sans({ 
+const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
-  weight : ['300','400', '500', '600', '700'], //폰트의 두께를 설정하는 부분. thin, regular, medium, semi-Bold, Bold
-  variable: '--font-sans'
+  weight: ["300", "400", "500", "600", "700"], //폰트의 두께를 설정하는 부분. thin, regular, medium, semi-Bold, Bold
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: "CareHeart",
+  title: "CareHealth",
   description: "A healthcare management system",
 };
 
@@ -21,12 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-        "min-h-screen bg-dark-300 font-sans antialiased",
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
           fontSans.variable
-          )}
-          >
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
+        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
